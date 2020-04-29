cf21be244c6cba081a1f54df2b165abe7ee5f25a
slush62 <steve@lushfamily.com>
2020-04-29 06:53:53 -0500

    first commit eclipse

--- src/main/java/com/peraton/wmss/noaa/capingest/CapingestApplication.java
+++ src/main/java/com/peraton/wmss/noaa/capingest/CapingestApplication.java
@@ -2,7 +2,9 @@ package com.peraton.wmss.noaa.capingest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import com.google.gson.*;
+import com.google.gson.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -21,7 +23,24 @@ public class CapingestApplication {
 		try {
 			HttpGet request = new HttpGet("https://api.weather.gov/alerts?active=true&status=actual");
 			CloseableHttpResponse response = httpClient.execute(request);
-			response.close();
+            try {
+
+                // Get HttpResponse Status
+                System.out.println(response.getProtocolVersion());              // HTTP/1.1
+                System.out.println(response.getStatusLine().getStatusCode());   // 200
+                System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+                System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    // return it as a String
+                    String result = EntityUtils.toString(entity);
+                    System.out.println(result);
+                }
+
+            } finally {
+                response.close();
+            }
 		} finally {
 			httpClient.close();
 		}
